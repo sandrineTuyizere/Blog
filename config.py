49907ef2919926f160e5e4dffb01c50b7c1c4731
Sandrine Tuyizere <sandrauyizere@gmail.com>
2020-12-06 20:27:16 +0200

    modifying tests

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     MAIL_USE_SSL=True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-     # simple mde  configurations
+    
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sandrine:7676@localhost/blog'
